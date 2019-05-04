2161e608085b0e8a99ca0904479699353d6feab7
Matheus Souza <matheusviegasdesouza@gmail.com>
2019-05-03 23:30:48 -0300

    Reestruturando as pastas e adicionando nodemailer pra recuperação de senha

--- src/app/controllers/AuthController.js
+++ src/app/controllers/AuthController.js
@@ -1,7 +1,7 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const authConfig = require("../config/auth");
+const authConfig = require("../../config/auth");
 
 function generateToken(params = {}) {
     return jwt.sign(params, authConfig.secret, {
